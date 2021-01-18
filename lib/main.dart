0204aa61bed04877746619803a5c643d74ec1c39
Honey Sonwani <honeysonwani88@gmail.com>
2021-01-18 22:12:32 +0530

    Use an external package `english-words`

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(Home());
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: "This is your first flutter app",
       home: Scaffold(
         appBar: AppBar(
           title: const Text("HomePage"),
         ),
-        body: const Center(
-          child: const Text("Welcome home"),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
